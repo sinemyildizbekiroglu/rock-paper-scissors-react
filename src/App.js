fec0be3ba690bad180b80d8c570ec25203f4f15b
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-11-09 09:51:07 +0100

    Update `App.js`

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 const App = () => {
   return (
     <div>
+      <h1>user choice is: </h1>
+      <h1>computer choice is: </h1>
+      <button
+        onClick={() => {
+          console.log("clicked")
+        }}
+      >
+        Rock
+      </button>
+      <button
+        onClick={() => {
+          console.log("clicked")
+        }}
+      >
+        Paper
+      </button>
+      <button
+        onClick={() => {
+          console.log("clicked")
+        }}
+      >
+        Scissors
+      </button>
     </div>
   )
 }
