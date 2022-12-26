8f800f80f22fedab33fb6798f4a8959433026cf4
sinemyildizbekiroglu <sinemyildizbekiroglu@gmail.com>
2022-12-26 09:51:07 +0100

    Update `App.js`

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 const App = () => {
   const [userChoice, setUserChoice] = useState(null)
+  const choices = ["rock", "paper", "scissors"]
 
   const handleClick = (value) => {
     setUserChoice(value)
@@ -9,11 +10,12 @@ const App = () => {
 
   return (
     <div>
-      <h1>user choice is: </h1>
+      <h1>user choice is: {userChoice} </h1>
       <h1>computer choice is: </h1>
-      <button onClick={() => handleClick("rock")}>Rock</button>
+      {/* <button onClick={() => handleClick("rock")}>Rock</button>
       <button onClick={() => handleClick("paper")}>Paper</button>
-      <button onClick={() => handleClick("scissors")}>Scissors</button>
+      <button onClick={() => handleClick("scissors")}>Scissors</button> */}
+      {choices.map((choice, index) => <button key={index}onClick={() => handleClick("choice")}>{choice}</button>)}
     </div>
   )
 }
